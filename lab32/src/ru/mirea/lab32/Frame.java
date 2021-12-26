ad8b8ef1428be3fd975846bead2b759d5417904d
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2021-12-26 21:27:51 +0300

    Ending Course

--- lab32/src/ru/mirea/lab32/Frame.java
+++ lab32/src/ru/mirea/lab32/Frame.java
@@ -1,16 +1,11 @@
 package ru.mirea.lab32;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class Frame extends JFrame {
     InternetOrdersManager internetOrdersManager = new InternetOrdersManager();
